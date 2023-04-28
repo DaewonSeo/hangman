db2a00af0b0aaa90424923d25a02043f83850ffa
daewonseo <saz0549@naver.com>
2023-04-28 15:50:37 +0900

    changed greeting message

--- hangman.py
+++ hangman.py
@@ -132,7 +132,7 @@ def checkWrongAnswer(missedLetters, secretWord):
 
 def main():
     """Main application entry point."""
-    print('H A N G M A N by ...')
+    print('H A N G M A N by Daewon')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
